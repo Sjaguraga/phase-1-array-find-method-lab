59121874610341b30647f37229061c6bfd5158b2
serref jaguraga <reef163@gmail.com>
2022-02-03 09:22:33 -0500

    commit tree 71118f5ece43c1f0b14afb696bb0784345b0d823

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+
+/*function superbowlWin(record){
+const result = record.find(record => record.result === "W");
+return !!result ? result.year : undefined
+}*/
+
+function superbowlwin(record) {
+return record.year === 'W';
+}
